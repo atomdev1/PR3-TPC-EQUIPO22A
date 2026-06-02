aeb5ee49ce5aade008e7e3186e827ba8523d9914
Atom Dev <tomas.oliveres@alumnos.frgp.utn.edu.ar>
2026-06-01 22:03:23 -0300

    Feat: Agregue clase Pago

---
+++ Dominio/Pago.cs
@@ -0,0 +1,18 @@
+﻿using Dominio.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pago
+    {
+        public int IdPago { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime FechaHora { get; set; }
+        public int IdReserva { get; set; }
+        public FormaPago FormaDePago { get; set; } // Uso del Enum
+    }
+}
